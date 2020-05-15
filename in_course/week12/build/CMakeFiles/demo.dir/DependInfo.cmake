54aa62beabc718478bb2f4dd9136cdb802af1f3e
KenSporger <ken.sporger@outlook.com>
2020-05-15 14:42:23 +0800

    add week12:gamma,equalizeHist

---
+++ in_course/week12/build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ken/Documents/Digital-Image-Processing-Course/in_course/week12/main.cpp" "/home/ken/Documents/Digital-Image-Processing-Course/in_course/week12/build/CMakeFiles/demo.dir/main.cpp.o"
+  "/home/ken/Documents/Digital-Image-Processing-Course/in_course/week12/src/equalizeHist.cpp" "/home/ken/Documents/Digital-Image-Processing-Course/in_course/week12/build/CMakeFiles/demo.dir/src/equalizeHist.cpp.o"
+  "/home/ken/Documents/Digital-Image-Processing-Course/in_course/week12/src/gamma.cpp" "/home/ken/Documents/Digital-Image-Processing-Course/in_course/week12/build/CMakeFiles/demo.dir/src/gamma.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  "/usr/local/opencv-3.4.2/include"
+  "/usr/local/opencv-3.4.2/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ in_course/week12/build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/main.cpp.o"
+  "CMakeFiles/demo.dir/src/gamma.cpp.o"
+  "CMakeFiles/demo.dir/src/equalizeHist.cpp.o"
+  "demo.pdb"
+  "demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
