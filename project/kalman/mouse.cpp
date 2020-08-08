dfe93eb8948527143560e54c1af245cbcd7b2bc1
kensporger <ken.sporger@outlook.com>
2020-08-08 20:41:01 +0800

    share_ptr;kalman

---
+++ project/kalman/mouse.cpp
@@ -0,0 +1,89 @@
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+using namespace cv;
+using namespace std;
+#define KALMAN true 
+ 
+//滤波效果：快速移动突然停止会有惯性存在
+//慢速移动的跟踪较好
+
+const int winHeight=600;
+const int winWidth=800;
+ 
+ 
+Point mousePosition= Point(winWidth>>1,winHeight>>1);
+Point mousePositionSample= mousePosition;
+
+// 获取鼠标实际位置
+//mouse event callback
+void mouseEvent(int event, int x, int y, int flags, void *param )
+{
+	if (event==CV_EVENT_MOUSEMOVE) {
+		mousePosition = Point(x,y);
+	}
+}
+
+//green for predict, blue for turth
+int main()
+{
+    RNG rng;
+	//1.kalman filter setup
+	const int stateNum=4;                                      //状态值4×1向量(x,y,△x,△y)
+	const int measureNum=2;                                    //测量值2×1向量(x,y)	
+	KalmanFilter KF(stateNum, measureNum, 0);	
+	KF.transitionMatrix = (Mat_<float>(4, 4) <<1,0,1,0,
+                                               0,1,0,1,
+                                               0,0,1,0,
+                                               0,0,0,1);  //转移矩阵A
+	setIdentity(KF.measurementMatrix);                                             //测量矩阵H
+	setIdentity(KF.processNoiseCov, Scalar::all(1e-5));                            //系统噪声方差矩阵Q
+	setIdentity(KF.measurementNoiseCov, Scalar::all(1e-1));                        //测量噪声方差矩阵R
+	setIdentity(KF.errorCovPost, Scalar::all(1));                                  //后验错误估计协方差矩阵P
+	rng.fill(KF.statePost,RNG::UNIFORM,0,winHeight>winWidth?winWidth:winHeight);   //初始状态值x(0)
+	Mat measurement = Mat::zeros(measureNum, 1, CV_32F);                           //初始测量值x'(0)，因为后面要更新这个值，所以必须先定义
+	
+	namedWindow("kalman");
+	setMouseCallback("kalman",mouseEvent);
+		
+	Mat image(winHeight,winWidth,CV_8UC3,Scalar(0));
+    Point predict_pt;
+    int cnt= 0;
+	while (1)
+	{
+		image.setTo(Scalar(255,255,255,0));
+		circle(image,mousePosition,5,Scalar(255,0,0),3); //current position with red	
+        	
+        if (++cnt == 1)
+        {
+
+		    //2.kalman prediction
+            Mat prediction = KF.predict();
+            predict_pt = Point(prediction.at<float>(0),prediction.at<float>(1) );   //预测值(x',y')
+    
+            //3.update measurement
+            measurement.at<float>(0) = (float)mousePosition.x;
+            measurement.at<float>(1) = (float)mousePosition.y;		
+    
+            //4.update
+            KF.correct(measurement);
+    
+            //draw 
+            // char buf[256];
+            // // sprintf_s(buf,256,"predicted position:(%3d,%3d)",predict_pt.x,predict_pt.y);
+            // putText(image,buf,Point(10,30),CV_FONT_HERSHEY_SCRIPT_COMPLEX,1,Scalar(0,0,0),1,8);
+            // // sprintf_s(buf,256,"current position :(%3d,%3d)",mousePosition.x,mousePosition.y);
+            // putText(image,buf,cvPoint(10,60),CV_FONT_HERSHEY_SCRIPT_COMPLEX,1,Scalar(0,0,0),1,8);
+            cnt = 0;
+            if (KALMAN)
+                mousePositionSample = predict_pt;
+            else
+                mousePositionSample = mousePosition;
+        }
+
+        circle(image,mousePositionSample,5,Scalar(0,255,0),3);    //predicted point with green
+		imshow("kalman", image);
+        waitKey(1);
+
+	}
+}
