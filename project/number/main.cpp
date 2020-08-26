d5f2fa203f71d977f06b3d74d0e7486b934f8824
KenSporger <ken.sporger@outlook.com>
2020-08-25 19:57:42 -0700

    only number

--- project/number/main.cpp
+++ project/number/main.cpp
@@ -11,8 +11,8 @@ using namespace cv;
 
 
 
-
-int main()
+//Tesseract 4.0最好在浅色背景上使用深色文本
+int demo1()
 {
     Mat img = imread("../../../img/ocr.jpg", 0);
     Rect numRoi;
@@ -20,7 +20,7 @@ int main()
     numRoi = selectROI(img);
     numMat = img(numRoi);
     numMat.copyTo(dst);
-    // threshold(numMat, dst, 100, 255,THRESH_BINARY);
+    threshold(numMat, dst, 200, 255,THRESH_BINARY_INV);
     imshow("roi", dst);
     waitKey(0);
 
@@ -32,37 +32,35 @@ int main()
     // Get the text
     char* out = tess.GetUTF8Text();
     std::cout << out << std::endl;
+}
 
+//only number
+int demo2()
+{
+    Mat img = imread("../../../img/ocr.jpg", 0);
+    Rect numRoi;
+    Mat numMat, dst;
+    numRoi = selectROI(img);
+    numMat = img(numRoi);
+    numMat.copyTo(dst);
+    threshold(numMat, dst, 200, 255,THRESH_BINARY_INV);
+    imshow("roi", dst);
+    waitKey(0);
 
-
+    tesseract::TessBaseAPI tess;
+    tess.Init(NULL, "eng");
+    tess.SetVariable("tessedit_char_whitelist", "0123456789");
+    tess.SetImage((uchar*)dst.data, dst.cols, dst.rows, 1, dst.cols);
+    // Get the text
+    char* out = tess.GetUTF8Text();
+    std::cout << out << std::endl;
 }
 
 
 
 
-// int main()
-// {
-//     char *outText;
-
-//     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-//     // Initialize tesseract-ocr with English, without specifying tessdata path
-//     if (api->Init(NULL, "eng")) {
-//         fprintf(stderr, "Could not initialize tesseract.\n");
-//         exit(1);
-//     }
-
-//     // Open input image with leptonica library
-//     Pix *image = pixRead("img/ocr.jpg");
-//     api->SetImage(image);
-//     // Get OCR result
-//     outText = api->GetUTF8Text();
-//     printf("OCR output:\n%s", outText);
-
-//     // Destroy used object and release memory
-//     api->End();
-//     delete api;
-//     delete [] outText;
-//     pixDestroy(&image);
-
-//     return 0;
-// }
+int main()
+{
+    // demo1();
+    demo2();
+}
